a560fa04a9ca877c9edc40a0c7ca4951ce926b6a
kyle65463 <kyle65463@gmail.com>
2021-07-11 12:40:29 +0800

    refactor: refactor next area

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { Input } from "./input.js";
 import { DisplayBox } from "./display_box.js"
 import { TetrominoFactory } from "./tetromino/tetromino_factory.js";
 import { HoldArea } from "./hold_area.js";
+import { NextArea } from "./next_area.js";
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -11,27 +12,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const board = new Board();
-
+const nextArea = new NextArea();
+const holdArea = new HoldArea();
 
 let timer = false;
 let movingTetromino = TetrominoFactory.randomCreate();
 movingTetromino.setInBoard();
-
-// Next area
-let nextTetrominos = []
-let nextTetrominoBoxes = [];
-let posStartY = 200;
-for (let i = 0; i < 3; i++) {
-    let tetromino = TetrominoFactory.randomCreate();
-    let [posX, posY] = [1000, posStartY + i * DisplayBox.getHeight()]
-    tetromino.setNotInBoard(posX, posY);
-    nextTetrominos.push(tetromino);
-    nextTetrominoBoxes.push(new DisplayBox(posX, posY));
-}
 let grids = [];
-
-// Hold area
-let holdArea = new HoldArea();
 
 function setNextTetrominos(nextTetrominos) {
     let i = 0;
@@ -50,40 +37,24 @@ function gameLoop() {
         for (let grid of movingTetromino.grids) {
             grids.push(grid);
         }
-        movingTetromino = nextTetrominos[0];
+        movingTetromino = nextArea.getNextTetromino();
         movingTetromino.setInBoard();
-
-        let newTetromino = TetrominoFactory.randomCreate();
-        nextTetrominos.splice(0, 1);
-        nextTetrominos.push(newTetromino);
-        setNextTetrominos(nextTetrominos);
     }
     board.update(grids);
     movingTetromino = holdArea.update(movingTetromino);
     if (!movingTetromino) {
-        movingTetromino = nextTetrominos[0];
+        movingTetromino = nextArea.getNextTetromino();
         movingTetromino.setInBoard();
-
-        let newTetromino = TetrominoFactory.randomCreate();
-        nextTetrominos.splice(0, 1);
-        nextTetrominos.push(newTetromino);
-        setNextTetrominos(nextTetrominos);
     }
+    nextArea.update();
 
     // Render
     board.render(ctx);
-    for (let grid of grids) {
+    for (let grid of grids)
         grid.render(ctx);
-    }
     movingTetromino.render(ctx);
-
-    for (let displayBox of nextTetrominoBoxes) {
-        displayBox.render(ctx);
-    }
-    for (let tetromino of nextTetrominos) {
-        tetromino.render(ctx);
-    }
     holdArea.render(ctx);
+    nextArea.render(ctx);
 
     // End
     timer = false;
